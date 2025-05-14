58032d4cfc29d405c8821458f81a47da207825b8
아정 <80907516+ajung7038@users.noreply.github.com>
2025-05-14 12:47:57 +0900

    백준 - 블랙잭

---
+++ Ajeong/블랙잭.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/2798
+
+public class boj_2798 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int max=0;
+		
+		st = new StringTokenizer(br.readLine());
+		int[] arr = new int[N];
+		
+		for (int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for (int i=0; i<N; i++) {
+			for (int j=1; j<N; j++) {
+				if (i==j) continue;
+				for (int k=2; k<N; k++) {
+					if (j==k || i==k) continue;
+					int sum = arr[i] + arr[j] + arr[k];
+					if (sum <= M && max < sum) max = sum;
+				}
+			}
+		}
+		
+		System.out.println(max);
+	}
+}
