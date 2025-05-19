68f401ee8e45f4bc008a7d8b14a5efd808009870
아정 <80907516+ajung7038@users.noreply.github.com>
2025-05-19 10:29:13 +0900

    백준 - Hashing

---
+++ Ajeong/Hashing.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+// https://www.acmicpc.net/problem/15829
+
+public class boj_15829 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		String str = br.readLine();
+		long sum = 0;
+		long pow=1;
+		
+		for (int i=0; i<N; i++) {
+			sum += ((str.charAt(i) - 96) * pow);
+			pow = (pow * 31) % 1234567891;
+		}
+		System.out.println(sum % 1234567891);
+	}
+}
