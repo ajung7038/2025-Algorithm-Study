b367470094f4fb9041981cb2abcb5f5c36b590b2
김혜림 <hyerim218@naver.com>
2025-06-27 22:45:25 +0900

    백준 - 큐

---
+++ Hyerim/큐.py
@@ -0,0 +1,33 @@
+N = int(input())
+queue=[]
+output=[]
+for i in range(N):
+    command = input().split()
+    cmd = command[0]
+    if cmd == 'push':
+        queue.append(command[1])
+    elif cmd == 'pop':
+        if len(queue) == 0:
+            output.append(-1)
+        else:
+            output.append(queue[0])
+            queue.pop(0)
+    elif cmd == 'size':
+        output.append(len(queue))
+    elif cmd == 'empty':
+        if len(queue) == 0:
+            output.append(1)
+        else:
+            output.append(0)
+    elif cmd == 'front':
+        if len(queue) == 0:
+            output.append(-1)
+        else:
+            output.append(queue[0])
+    elif cmd =='back':
+        if len(queue) == 0:
+            output.append(-1)
+        else:
+            output.append(queue[-1])
+for j in output:
+    print(j)
